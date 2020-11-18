fc75a1b40f4bc8ad2f67578f3674804a2a58996d
karinjohanson <karin.kikas@gmail.com>
2020-11-18 13:15:15 +0200

    18.11 tunnitöö

--- start.js
+++ start.js
@@ -1,9 +1,11 @@
 (function($) {
-$.getJSON('https://jsonplaceholder.typicode.com/users').done(function(people) {
+//Kasutame välist API-t saamaks välist infot
+/**$.getJSON('https://jsonplaceholder.typicode.com/users').done(function(people) {
     console.log(people);
     for (person in people) {
         console.log(person);
         $('ul').append('<li>' + person + '</li>');
     }
 });
+*/
 })(jQuery)
